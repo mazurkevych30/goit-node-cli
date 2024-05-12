a47c0761983f38f47659c0315980a34f5b64913e
mazurkevych30 <132198562+mazurkevych30@users.noreply.github.com>
2024-05-12 23:48:52 +0100

    add package.json, index.js, contacts.js, gitignore

---
+++ contacts.js
@@ -0,0 +1,25 @@
+import fs from "fs/promises";
+import path from "path";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+export { listContacts, getContactById };
---
+++ index.js
@@ -0,0 +1,40 @@
+import { program } from "commander";
+import { listContacts, getContactById } from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const getContact = await getContactById(id);
+      console.log(getContact);
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
